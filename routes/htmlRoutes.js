9210df986faf65bee6b02a61da7a818e0e644318
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-08 17:10:27 -0700

    finished express html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+// DEPENDENCIES
+const path = require("path");
+
+// EXPORT THE ROUTING TO BE PULLED BY INDEX.JS
+module.exports = function(app) {
+    app.get("/notes", function(req,res) {
+        res.sendFile(path.join(__dirname,"../public/notes/html"));
+    });
+
+    app.get("*", function(req,res) {
+        res.sendFile(path.join(__dirname,"../public/index.html"));
+    });
+}
